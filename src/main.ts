1229623f0f4693ccde721dcf74409cd9c57bbbf4
付志强 <zhiqiangfu6@gmail.com>
2022-12-05 18:00:17 +0800

    chore: add code travser

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,18 @@ import traverse from "@babel/traverse";
 /**
  * 业务逻辑
  */
-export function getFunctionNode() {
-	const code = `
-		function getName(){
-			return 'name'
-		}
-		function getDemo(){
-			return 'name'
-		}
-	`;
+export function getFunctionNode(code: string) {
+
+	let functionNode;
 	const ast = parse(code);
 	traverse(ast, {
 		FunctionDeclaration(path) {
-			console.log(path.node);
+			functionNode = {
+				name: path.node.id?.name,
+				start: path.node.loc?.start,
+				end: path.node.loc?.end
+			};
 		}
 	});
+	return functionNode;
 }
--- test/main.spec.ts
+++ test/main.spec.ts
@@ -1,5 +1,24 @@
 import { test, expect } from 'vitest';
 import { getFunctionNode } from '../src/main';
-test('init', async () => {
-    getFunctionNode();
+test('init', () => {
+    const code = `
+		function getName(){
+			return 'name'
+		}
+		function getDemo(){
+			return 'name'
+		}
+	`;
+
+    const functionNode = getFunctionNode(code);
+    expect(functionNode).toEqual({
+        name: "getName",
+        start: {
+            line: 2, column: 2, index: 3
+        },
+        end: {
+            line: 4, column: 3, index: 43
+        },
+
+    });
 });
